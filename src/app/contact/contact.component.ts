4c2a8b9cd04deb94320dc8d9dbe589720dc7e120
nadun96 <nadunudaraka@gmail.com>
2025-04-21 18:01:59 +0530

    init

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'app-contact',
+  templateUrl:'./contact.componenet.html',
+  styleUrl:'./contact.componenet.scss',
+  standalone: true
+})
+
+export class ContactComponenet{
+
+}
