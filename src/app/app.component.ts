2a7d526f929fa6c0c0bd1f41944f911d8be3f3d7
nadun96 <nadunudaraka@gmail.com>
2025-04-25 14:05:13 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule,NgIf } from '@angular/common';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent{
-  title = 'NADUN';
-  width = 500;
-  fontSize = "30px";
-  isActive = true;
-  imageLink: string = "https://cdn.pixabay.com/photo/2025/04/08/00/45/pattern-9520292_640.jpg";
-  changeFontSize() {
-    this.fontSize = this.fontSize === "30px" ? "50px" : "30px";
-  }
-  printData(value:any) {
-    this.title = value;
-  }
+  state = false;
 }
