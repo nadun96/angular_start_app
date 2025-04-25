f85cb71d8eb4a52a8b5e0e49902c36ab6c2ace5b
nadun96 <nadunudaraka@gmail.com>
2025-04-25 11:12:28 +0530

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ContactComponent} from './contact/contact.component';
-import {WorksComponent} from './works/works.component';
-import {AboutComponent} from './about/about.component';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactComponent, WorksComponent,AboutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent{
   title = 'NADUN';
-  width="300px";
-  imageLink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1zwhySGCEBxRRFYIcQgvOLOpRGqrT3d7Qng&s';
+  width=500;
+  imageLink :string = "https://cdn.pixabay.com/photo/2025/04/08/00/45/pattern-9520292_640.jpg";
 }
