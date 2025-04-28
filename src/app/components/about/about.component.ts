7573f26e0cc5095fffa12d975772ed69fed4b26d
nadun96 <nadunudaraka@gmail.com>
2025-04-28 15:53:59 +0530

    Debounce pipes

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,32 +1,26 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {debounceTime, Observable} from 'rxjs';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit{
-  // Observable Demonstration
-  myObserver= new Observable((observer) => {
-    console.log('Observable Started');
-    setTimeout(()=> observer.next(1), 1000);
-    setTimeout(()=> observer.next(2), 2000);
-    setTimeout(()=> observer.next(3), 3000);
-    setTimeout(()=> observer.next(4), 4000);
-    setTimeout(()=> observer.next(5), 5000);
-    setTimeout(()=> observer.next(6), 6000);
-    console.log('Observable Ended');
-  })
+
+  searchText = new FormControl();
   ngOnInit() {
-    this.myObserver.subscribe(
-      {
-        next:(data)=> console.log(data),
-        error:(error)=> console.log(error),
-        complete:()=> console.log('Completed'),
-      }
-    )
-    console.log('AboutComponent initialized');
+   /* this.searchText.valueChanges.subscribe(
+     ()=>{
+     console.log('searchText', this.searchText.value);
+     }
+   )*/
+    this.searchText.valueChanges.pipe(
+      debounceTime(3000)
+    ).subscribe((value)=>console.log(value))
   }
 }
