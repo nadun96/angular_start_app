fb8980fbc4b2f087dfd1b14e0dd3f06ae1152567
nadun96 <nadunudaraka@gmail.com>
2025-04-28 15:00:37 +0530

    Observable and ngOninit

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit{
+  // Observable Demonstration
+  myObserver= new Observable((observer) => {
+    console.log('Observable Started');
+    setTimeout(()=> observer.next(1), 1000);
+    setTimeout(()=> observer.next(2), 2000);
+    setTimeout(()=> observer.next(3), 3000);
+    setTimeout(()=> observer.next(4), 4000);
+    setTimeout(()=> observer.next(5), 5000);
+    setTimeout(()=> observer.next(6), 6000);
+    console.log('Observable Ended');
+  })
+  ngOnInit() {
+    this.myObserver.subscribe(
+      {
+        next:(data)=> console.log(data),
+        error:(error)=> console.log(error),
+        complete:()=> console.log('Completed'),
+      }
+    )
+    console.log('AboutComponent initialized');
+  }
 }
