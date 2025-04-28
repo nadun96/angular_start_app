3b2c924c3194f3411bb6d0a2b07efd5ecac3d38c
nadun96 <nadunudaraka@gmail.com>
2025-04-28 17:01:41 +0530

    Pipes introduction

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,34 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import {BehaviorSubject, debounceTime, Observable, Subject} from 'rxjs';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {CurrencyPipe, DatePipe, SlicePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-about',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    UpperCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    SlicePipe
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit{
 
-  searchText = new FormControl();
+  text = "Hi this is angular sample app";
+  date = new Date();
+  salary = 55000;
+  name = "Nadun Udaraka";
   ngOnInit() {
-    // const subject = new Subject<string>();
-    // subject.subscribe((value)=>{console.log(`Subscriber 1 ${value}`)})
-    // subject.next("Text 1"); // ---- called as emiiting values
-    // subject.next("Text 2");
-    // subject.next("Text 3");
-    // subject.subscribe((value) => {console.log(`Subscriber 2 ${value}`)})
-    // subject.next("Text 4");
-
-    const bsubject = new BehaviorSubject<string>("initial");
-    bsubject.subscribe((value)=>{console.log(`Subscriber 1 ${value}`)})
-    bsubject.next("Text 1"); // ---- called as emiiting values
-    bsubject.next("Text 2");
-    bsubject.next("Text 3"); // after the emmit process Subscriber two can access the last value
-    bsubject.subscribe((value) => {console.log(`Subscriber 2 ${value}`)})
-    bsubject.next("Text 4");
-
+    console.log(this.text);
+    console.log(this.date);
+    console.log(this.salary);
+    console.log(this.name);
   }
 }
