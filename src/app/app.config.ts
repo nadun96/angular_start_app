a952bd011e0e93c962aadc9b31f29d7b25464871
nadun96 <nadunudaraka@gmail.com>
2025-04-28 16:40:37 +0530

    use # Routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,21 @@ import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/ht
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {httpManagerInterceptor} from './interceptor/http-manager.interceptor';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    // TO call Http request configure interceptors
     provideHttpClient(
       withInterceptors([httpManagerInterceptor]),
       withFetch()
-    )
+    ),
+    // enable # routing
+    {
+      provide: LocationStrategy, useClass:HashLocationStrategy
+    }
   ]
 };
