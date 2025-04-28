a1392c66fdc666c4d2c4f39d6214022af3852549
nadun96 <nadunudaraka@gmail.com>
2025-04-28 16:36:38 +0530

    Subjects and Behavioral SUbjects

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {debounceTime, Observable, Subject} from 'rxjs';
+import {BehaviorSubject, debounceTime, Observable, Subject} from 'rxjs';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
@@ -14,12 +14,21 @@ export class AboutComponent implements OnInit{
 
   searchText = new FormControl();
   ngOnInit() {
-    const subject = new Subject<string>();
-    subject.subscribe((value)=>{console.log(`Subscriber 1 ${value}`)})
-    subject.next("Text 1");
-    subject.next("Text 2");
-    subject.next("Text 3");
-    subject.subscribe((value) => {console.log(`Subscriber 2 ${value}`)})
-    subject.next("Text 4");
+    // const subject = new Subject<string>();
+    // subject.subscribe((value)=>{console.log(`Subscriber 1 ${value}`)})
+    // subject.next("Text 1"); // ---- called as emiiting values
+    // subject.next("Text 2");
+    // subject.next("Text 3");
+    // subject.subscribe((value) => {console.log(`Subscriber 2 ${value}`)})
+    // subject.next("Text 4");
+
+    const bsubject = new BehaviorSubject<string>("initial");
+    bsubject.subscribe((value)=>{console.log(`Subscriber 1 ${value}`)})
+    bsubject.next("Text 1"); // ---- called as emiiting values
+    bsubject.next("Text 2");
+    bsubject.next("Text 3"); // after the emmit process Subscriber two can access the last value
+    bsubject.subscribe((value) => {console.log(`Subscriber 2 ${value}`)})
+    bsubject.next("Text 4");
+
   }
 }
