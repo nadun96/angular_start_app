3fd5eab4914a768e3faedf450327fc8dc29a4152
nadun96 <nadunudaraka@gmail.com>
2025-05-02 16:30:48 +0530

    structural directive

--- student/student.component.ts
+++ student/student.component.ts
@@ -1,16 +1,27 @@
 import {Component} from '@angular/core';
 import {HighLightDirective} from './directive/HighLightDirective';
+import {NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
 
 @Component({
   selector: 'app-student',
-  template: `<h2 appHighlight="appHighLight"> HI My Name is : {{ myName }}</h2>`,
+  template: `<h2 appHighlight="appHighLight"> HI My Name is : {{ myName }}</h2>
+  <div [ngSwitch]="color">
+    <p *ngSwitchCase="'red'">Red</p>
+    <p *ngSwitchCase="'black'">Black</p>
+    <p *ngSwitchCase="'blue'">Blue</p>
+    <p *ngSwitchDefault> Not Defined</p>
+  </div>`,
   imports: [
-    HighLightDirective
+    HighLightDirective,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault
   ],
-  // styles: [`h2 {
-  //   color: blue
-  // }`]
+  styles: [`h2 {
+    color: blue
+  }`]
 })
 export class StudentComponent{
   myName : string = 'John Doe';
+  color : string = 'Orange';
 }
