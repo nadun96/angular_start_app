e7cef45fe549a1edf37628f7145b6bc07c29f198
nadun96 <nadunudaraka@gmail.com>
2025-05-06 12:32:09 +0530

    template driven approch forms

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  customer= {
+    name:'',
+    address: '',
+    salary: '',
+    age:  '',
+  }
+
+  submitData() {
+    console.log(this.customer);
+  }
 
 }
